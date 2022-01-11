4ed2281d325e89cbc81c3d4624a8a879ce770141
dev <gmk.hussain@gmail.com>
2022-01-11 15:09:16 +0500

    Routes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,25 @@
+import React from 'react'
+
+// 3rd Party Lib
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+
 import './App.css';
+
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          App works
-        </p>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/contact">
+            <p>Contact works</p>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ react-app/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+
+        return (
+            <section>
+               Home works
+            </section>
+          )
+}
+
+export default Home
